98821f55f513c8dacc58520dd96b06043707a4ec
Jenny1905 <kumbhalkarshiwani@gmail.com>
2023-04-04 15:50:12 +0530

    second commit

---
+++ dayone/src/org/tnsif/introduction/DataTypesDemo.java
@@ -0,0 +1,30 @@
+// Program to demonstrate on various data types
+package org.tnsif.introduction;
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+		
+		//integer will give us the values in whole number
+		//float and double will gives us the velue in decimal point
+		/*if we use in programming division '/' , it prints only
+		 * quotient value and modulo will print '%' , prints
+		 * remainder value 
+		 */
+		int num1=5/2;
+		float num2=103/45;
+		/*if we use d after any number for double type it will give
+		 * the precision upto 16 decimal places
+		 */
+		double num3=10d/3d;
+		/*if we use f after any number for float type it will give
+		 * the precision upto 7 decimal places
+		 */
+		float  num4=14f/3f;
+		System.out.println("The value1 is: "+num1);
+		System.out.println("The value2 is: "+num2);
+		System.out.println("The value3 is: "+num3);
+		System.out.println("The value4 is: "+num4);
+
+	}
+
+}
--- dayone/src/org/tnsif/introduction/FirstProgram.java
+++ dayone/src/org/tnsif/introduction/FirstProgram.java
@@ -1,5 +1,6 @@
 package org.tnsif.introduction;
 //Program to demonstrate on simple java program
+
 public class FirstProgram {
 
 	public static void main(String[] args) {
