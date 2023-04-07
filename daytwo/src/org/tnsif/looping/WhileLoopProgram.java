b5a4e0acd6e268b41db3a535ca83e2f4939f51a8
Jenny1905 <kumbhalkarshiwani@gmail.com>
2023-04-07 16:47:51 +0530

    third commit

---
+++ daytwo/src/org/tnsif/looping/WhileLoopProgram.java
@@ -0,0 +1,21 @@
+//program to demonstrate on while loop
+package org.tnsif.looping;
+import java.util.Scanner;
+
+public class WhileLoopProgram {
+
+	public static void main(String[] args) {
+		//print 1 to N
+		int i=1;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter a value of N: ");
+		int N=s.nextInt();
+		while(i<=N)
+		{
+			System.out.print(i+" ");
+			i++;
+		}
+
+	}
+
+}
