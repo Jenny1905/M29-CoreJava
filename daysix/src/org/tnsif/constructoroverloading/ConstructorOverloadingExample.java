b6170c1976ff12de76e668e770640736c2e00526
Jenny1905 <kumbhalkarshiwani@gmail.com>
2023-04-24 17:44:52 +0530

    second commit

---
+++ daysix/src/org/tnsif/constructoroverloading/ConstructorOverloadingExample.java
@@ -0,0 +1,22 @@
+//program to demonstrate on constructor overloading
+package org.tnsif.constructoroverloading;
+public class ConstructorOverloadingExample {
+	
+	private int x;
+	private int y;
+	
+	//constructor overloading
+	public ConstructorOverloadingExample()
+	{
+		System.out.println("Default constructor");
+	}
+	public ConstructorOverloadingExample(int x)
+	{
+		System.out.println("Parameterized constructor"+x);
+	}
+	public ConstructorOverloadingExample(int x,int y)
+	{
+		System.out.println("Parameterized constructor"+x
+				+" "+y);
+	}
+}
